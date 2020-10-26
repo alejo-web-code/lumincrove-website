0b753c1cabd43ce24ace84c1c7e3c1a5e596487d
alejo-web-code <66263039+alejo-web-code@users.noreply.github.com>
2020-10-26 09:39:05 -0400

    create template config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const { createPage } = actions;
+    const templates = {
+        Services: path.resolve(`src/templates/services.js`),
+    };
+
+    const allPages = await graphql(`
+		{
+			allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+				edges {
+					node {
+						id
+						frontmatter {
+							path
+							template
+						}
+					}
+				}
+			}
+		}
+	`);
+
+    // Handle errors
+    if (allPages.errors) {
+        reporter.panicOnBuild(`Error while running GraphQL query.`);
+        return;
+    }
+
+    allPages.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+            path: node.frontmatter.path,
+            component: templates[node.frontmatter.template],
+            context: {} // additional data can be passed via context
+        });
+    });
+};
+
+// turn off sourcemaps in production build no longer works
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+    // If production JavaScript and CSS build
+    if (stage === 'build-javascript') {
+        // Turn off source maps
+        actions.setWebpackConfig({
+            devtool: false
+        });
+    }
+};
---
+++ src/templates/services.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Template(
+    // this prop will be injected by the GraphQL query below.
+) {
+
+}
