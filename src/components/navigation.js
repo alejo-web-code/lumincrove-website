2451d9526e059d12804de83597606924e13beeae
morghullis <tonymonzon94@gmail.com>
2020-10-27 15:12:10 -0700

    navigations

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,39 @@
 import React from "react"
+import { Link } from "gatsby"
 
 function Navigation() {
     return (
-        <div>
-            Navegacion
-        </div>
+        <section>
+          <div className="hide-desktop w-5">
+              <button>
+                  <i className="menu-icon"></i>
+              </button>
+          </div>
+          <div className="flex between align-center">
+              <div>
+                  <img src="../../static/assets/logo.png"/>
+              </div>
+              <ul className="hide-mobile">
+                    <li className="padding-small">
+                        <Link to="#">Our Services</Link>
+                        <div className="hide">
+                            <p>Services 1</p>
+                            <p>Services 2</p>
+                            <p>Services 3</p>
+                        </div>
+                    </li>
+                    <li className="padding-small">
+                        <Link to="#">Terms Of Use</Link>
+                    </li>
+                    <li className="padding-small">
+                        <Link to="#">Privaci Policy</Link>
+                    </li>
+                    <li className="padding-small">
+                        <Link to="#">Blog</Link>
+                    </li>
+              </ul>
+          </div>
+        </section>
     )
 }
 
