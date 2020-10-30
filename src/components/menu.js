769595b5984357207c6482152439eab7091539b4
alejo-web-code <66263039+alejo-web-code@users.noreply.github.com>
2020-10-29 22:54:16 -0400

    menu mobile

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 
 function Menu(props) {
     const { openMenu } = props;
+
+    let [serviceMenu, setServiceMenu] = useState('hiden');
     let listClass;
+
+    const openServices = () => {
+        setServiceMenu('show')
+    }
 
     if (openMenu) {
         listClass = "nav-menu text-center"
@@ -13,25 +19,22 @@ function Menu(props) {
     }
 
     return (
-        <ul className={listClass}>
-            <li className="padding-small">
-                <Link to="#">Our Services</Link>
-                <div className="hide">
-                    <p>Services 1</p>
-                    <p>Services 2</p>
-                    <p>Services 3</p>
-                </div>
-            </li>
-            <li className="padding-small">
-                <Link to="#">Terms Of Use</Link>
-            </li>
-            <li className="padding-small">
-                <Link to="#">Privaci Policy</Link>
-            </li>
-            <li className="padding-small">
-                <Link to="#">Blog</Link>
-            </li>
-        </ul>
+        <div className={listClass}>
+            <ul>
+                <li className="padding-small">
+                    <span onClick={openServices}>Our Services</span>
+                </li>
+                <li className="padding-small">
+                    <Link to="#">Terms Of Use</Link>
+                </li>
+                <li className="padding-small">
+                    <Link to="#">Privaci Policy</Link>
+                </li>
+                <li className="padding-small">
+                    <Link to="#">Blog</Link>
+                </li>
+            </ul>
+        </div>
     )
 }
 
