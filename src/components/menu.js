0751a1c069e6d8dc1a7a2221afec232c30a4a847
alejo-web-code <66263039+alejo-web-code@users.noreply.github.com>
2020-10-30 14:09:07 -0400

    navigation

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,32 +6,61 @@ function Menu(props) {
 
     let [serviceMenu, setServiceMenu] = useState('hiden');
     let listClass;
+    let menuClass;
+    let submenuClass;
 
     const openServices = () => {
         setServiceMenu('show')
     }
 
-    if (openMenu) {
-        listClass = "nav-menu text-center"
+    const closeServices = () => {
+        setServiceMenu('hiden')
+    }
+
+    if (serviceMenu === 'hiden') {
+        menuClass = "text-center menu"
+        submenuClass = "text-center menu out"
     }
     else {
-        listClass = "nav-menu text-center close"
+        menuClass = "text-center menu out"
+        submenuClass = " text-center menu"
+    }
+
+    if (openMenu) {
+        listClass = "nav-menu text-center flex center"
+    }
+    else {
+        listClass = "nav-menu text-center flex center close"
     }
 
     return (
         <div className={listClass}>
-            <ul>
-                <li className="padding-small">
+            <ul className={menuClass}>
+                <li className="menu-item padding-small">
                     <span onClick={openServices}>Our Services</span>
                 </li>
-                <li className="padding-small">
+                <li className="menu-item padding-small">
                     <Link to="#">Terms Of Use</Link>
                 </li>
-                <li className="padding-small">
+                <li className="menu-item padding-small">
                     <Link to="#">Privaci Policy</Link>
                 </li>
-                <li className="padding-small">
+                <li className="menu-item padding-small">
                     <Link to="#">Blog</Link>
+                </li>
+            </ul>
+            <ul className={submenuClass}>
+                <li className="menu-item padding-small">
+                    <span onClick={closeServices}>Our Services</span>
+                </li>
+                <li className="menu-item padding-small">
+                    <Link to="#">Service 1</Link>
+                </li>
+                <li className="menu-item padding-small">
+                    <Link to="#">Service 2</Link>
+                </li>
+                <li className="menu-item padding-small">
+                    <Link to="#">Service 3</Link>
                 </li>
             </ul>
         </div>
