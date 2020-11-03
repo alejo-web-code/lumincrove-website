2e75e647755240865dd239a60b406f3f3bd94abe
alejo-web-code <66263039+alejo-web-code@users.noreply.github.com>
2020-11-02 21:11:54 -0500

    home carousel ok

--- src/components/carousel/infinite.js
+++ src/components/carousel/infinite.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState, useRef } from 'react';
 import { Link } from 'gatsby';
 import BackgroundImage from 'gatsby-background-image';
 import Img from 'gatsby-image';
@@ -6,34 +6,95 @@ import '../../styles/carousel.scss'
 
 function CarouselInfinite(props) {
     const { items } = props;
-
+    const trackRef = useRef(null);
     const [trackState, setTrackState] = useReducer((state, newState) => ({ ...state, ...newState }), {
+        prevIndex: 5,
         trackIndex: 0,
-        selectedTrack: items[0]
+        selectedTrack: items[0],
+        direction: 0
     });
 
     let itemClass = 'container';
 
-    const currentSlide = (index) => {
+    const slideRight = (index) => {
+        if (trackState.direction === -1) {
+            trackRef.current.className = "track flex start"
+        }
+        trackRef.current.style.transform = 'translateX(-375px)';
+        let indexBefore = trackState.trackIndex;
+
+        if (trackState.prevIndex === 5) {
+            setTrackState({
+                prevIndex: 0,
+                trackIndex: index,
+                selectedTrack: items[index],
+                direction: 1
+            });
+        }
+        else {
+            setTrackState({
+                prevIndex: indexBefore,
+                trackIndex: index,
+                selectedTrack: items[index],
+                direction: 1
+            });
+        }
+    }
+
+    const slideLeft = (index) => {
+        if (trackState.direction === 1) {
+            trackRef.current.className = "track flex end"
+        }
+        trackRef.current.style.transform = 'translateX(375px)'
+        let indexBefore = trackState.trackIndex;
+
         setTrackState({
+            prevIndex: indexBefore,
             trackIndex: index,
-            selectedTrack: items[index]
+            selectedTrack: items[index],
+            direction: -1
         });
+
+    }
+
+    const endTranslate = (e) => {
+        let track = e.target;
+
+        track.style.transition = 'none';
+        track.style['-webkit-transform'] = 'translate(0)';
+        setTimeout(() => {
+            track.style.transition = 'all 0.5s';
+        });
+        setTrackState({
+            prevIndex: 5
+        });
+    }
+
+    const currentSlide = (index) => {
+        if (index < trackState.trackIndex) {
+            slideLeft(index)
+        }
+        else if (index > trackState.trackIndex) {
+            slideRight(index)
+        }
     };
 
     return (
         <div className="carousel">
-            <div className="track">
+            <div className="track flex start" ref={trackRef} onTransitionEnd={endTranslate}>
                 {items.map((item, index) => {
                     const specialStyles = {};
                     let titleClass = item.style ? "color-secondary inline-block" : "color-primary inline-block";
 
-                    if (index === trackState.selectedTrack.index) {
-                        specialStyles.display = 'block';
-                        itemClass += ' fade'
+                    if (index === trackState.trackIndex) {
+                        itemClass += ' active'
                     }
                     else {
-                        specialStyles.display = 'none';
+                        itemClass = 'container';
+                    }
+
+                    if (trackState.prevIndex !== 5 && index === trackState.prevIndex) {
+                        itemClass += ' active'
                     }
 
                     return (
