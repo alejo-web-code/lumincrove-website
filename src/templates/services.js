76f72870b5d92a89199ef1a0bfb6cdbaccb69eb5
morghullis <tonymonzon94@gmail.com>
2020-11-25 13:32:27 -0800

    service page sticky bottom

--- src/templates/services.js
+++ src/templates/services.js
@@ -1,5 +1,5 @@
 import { graphql } from 'gatsby';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useReducer, useEffect } from 'react';
 import Layout from '../components/layout';
 import BackgroundImage from 'gatsby-background-image';
 import Helmet from 'react-helmet';
@@ -13,30 +13,45 @@ export default function Template({
     const { frontmatter } = markdownRemark;
     const { title, description, thumbnail, headingTitle, subtitle, services } = frontmatter;
     let backgroundHeader;
+    let component;
     let standardRef = useRef(null);
     let premiumRef = useRef(null);
     let customRef = useRef(null);
 
-    const [tabStyle, setTabStyle] = useState('premium');
+    const [state, setState] = useReducer((state, newState) => ({ ...state, ...newState }), {
+        tabStyle: 'premium',
+        color: 'cta'
+    });
+
+    const [viewportWidth, setViewportWidth] = useState('')
 
     const selectedTab = (e) => {
         if (e.currentTarget.id === 'premium') {
-            premiumRef.current.className = "offers-tabs selected";
-            standardRef.current.className = "offers-tabs";
-            customRef.current.className = "offers-tabs";
-            setTabStyle('premium')
+            premiumRef.current.className = `offers-tabs selected cta`;
+            standardRef.current.className = `offers-tabs cta`;
+            customRef.current.className = `offers-tabs cta`;
+            setState({
+                tabStyle: 'premium',
+                color: 'cta'
+            })
         }
         else if (e.currentTarget.id === 'standard') {
-            premiumRef.current.className = "offers-tabs";
-            standardRef.current.className = "offers-tabs selected";
-            customRef.current.className = "offers-tabs";
-            setTabStyle('standard')
+            premiumRef.current.className = `offers-tabs primary`;
+            standardRef.current.className = `offers-tabs selected primary`;
+            customRef.current.className = `offers-tabs primary`;
+            setState({
+                tabStyle: 'standard',
+                color: 'primary'
+            })
         }
         else if (e.currentTarget.id === 'custom') {
-            premiumRef.current.className = "offers-tabs";
-            standardRef.current.className = "offers-tabs";
-            customRef.current.className = "offers-tabs selected";
-            setTabStyle('custom')
+            premiumRef.current.className = `offers-tabs dark`;
+            standardRef.current.className = `offers-tabs dark`;
+            customRef.current.className = `offers-tabs selected dark`;
+            setState({
+                tabStyle: 'custom',
+                color: 'dark'
+            })
         }
     }
 
@@ -75,60 +90,70 @@ export default function Template({
                     <p>{subtitle}</p>
                 </div>
                 <div>
-                    <div className="">
-                        <div id="standard" ref={standardRef} className="offers-tabs" onClick={selectedTab}>
-                            <span className="">Standard</span>
-                        </div>
-                        <div id="premium" ref={premiumRef} className="offers-tabs selected" onClick={selectedTab}>
-                            <span className="">Premium</span>
-                        </div>
-                        <div id="custom" ref={customRef} className="offers-tabs" onClick={selectedTab}>
-                            <span className="">Custom</span>
-                        </div>
+                <div className="">
+                    <div id="standard" ref={standardRef} className="offers-tabs cta" onClick={selectedTab}>
+                        <span className="">Standard</span>
                     </div>
-                    <div className="service-list padding">
-                        {services.map((service, index) => {
-                            return (
-                                <div key={index}>
-                                    <h4 className="service-list-heading text-bold">{service.title}</h4>
-                                    <ul className="padding-top-small padding-bottom-small">
-                                        {service.items.map((element, index2) => {
-                                            let listItemClass = "list-items margin-bottom-tiny";
-                                            let selectValue = '';
-
-                                            if (tabStyle === "standard" && element.premium === false) {
-                                                listItemClass += " available"
-                                            }
-                                            else if (tabStyle === "premium") {
-                                                listItemClass += " available"
-                                            }
-                                            else if (tabStyle === "custom") {
-                                                if (!element.premium) {
-                                                    listItemClass += " available"
-                                                }
-                                                else {
-                                                    listItemClass = "list-item-selectable margin-bottom-tiny"
-                                                    selectValue = (
-                                                        <input type="checkbox" className="input" onClick={customizeService} />
-                                                    )
-                                                }
-                                            }
-
-                                            if (index === 0 && index2 === 0) {
-                                                listItemClass += " text-bold"
-                                            }
-
-                                            return (
-                                                <li key={index2} className={listItemClass}>{selectValue}{element.name}</li>
-                                            )
-                                        })}
-                                    </ul>
-                                </div>
-                            )
-                        })}
+                    <div id="premium" ref={premiumRef} className="offers-tabs selected cta" onClick={selectedTab}>
+                        <span className="">Premium</span>
                     </div>
-
+                    <div id="custom" ref={customRef} className="offers-tabs cta" onClick={selectedTab}>
+                        <span className="">Custom</span>
+                    </div>
                 </div>
+                <div className={`service-list ${state.color} padding`}>
+                    {services.map((service, index) => {
+                        return (
+                            <div key={index}>
+                                <h4 className={`service-list-heading ${state.color} text-bold`}>{service.title}</h4>
+                                <ul className="padding-top-small padding-bottom-small">
+                                    {service.items.map((element, index2) => {
+                                        let listItemClass = "list-items margin-bottom-tiny";
+                                        let selectValue = '';
+
+                                        if (state.tabStyle === "standard" && element.premium === false) {
+                                            listItemClass += " available"
+                                        }
+                                        else if (state.tabStyle === "premium") {
+                                            listItemClass += " available"
+                                        }
+                                        else if (state.tabStyle === "custom") {
+                                            if (!element.premium) {
+                                                listItemClass += " available"
+                                            }
+                                            else {
+                                                listItemClass = "list-item-selectable margin-bottom-tiny"
+                                                selectValue = (
+                                                    <input type="checkbox" className="input" onClick={customizeService} />
+                                                )
+                                            }
+                                        }
+
+                                        if (index === 0 && index2 === 0) {
+                                            listItemClass += " text-bold"
+                                        }
+
+                                        return (
+                                            <li key={index2} className={listItemClass}>{selectValue}{element.name}</li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            </section>
+            <section className="sticky botton margin-bottom-small">
+                    <div className={`margin-left border-radius margin-right bg-${state.color}`}>
+                        <h2 className="text-reversed text-semibold text-center">$ 300000</h2>
+                    </div>
+                    <div className="bg-white-light padding-bottom-small">
+                        <p className="text-semibold text-center padding-small">Saves $ 12000/year</p>
+                        <div className="text-center" id="hire-button">
+                            <button className="button cta">Hire Us!</button>
+                        </div>
+                    </div>
             </section>
         </Layout>
     )
